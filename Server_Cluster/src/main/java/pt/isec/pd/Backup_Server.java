a49f63f121a4d70d6df3f19b233b3d397baf7f47
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2023-10-14 16:57:30 +0100

    server_cluster project initialized

---
+++ Server_Cluster/src/main/java/pt/isec/pd/Backup_Server.java
@@ -0,0 +1,7 @@
+package pt.isec.pd;
+
+public class Backup_Server {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Server_Cluster/src/main/java/pt/isec/pd/Main_Server.java
@@ -0,0 +1,8 @@
+package pt.isec.pd;
+
+
+public class Main_Server {
+    public static void main(String[] args) {
+
+    }
+}
