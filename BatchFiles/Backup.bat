7212172f09927dd547d37a9e61d3d40d707d85e7
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2023-11-17 19:10:28 +0000

    Jar's created

---
+++ BatchFiles/Backup.bat
@@ -0,0 +1,4 @@
+@echo off
+mkdir default_backup
+java -jar ./jars/Backup.jar ./default_backup
+pause
---
+++ BatchFiles/Client.bat
@@ -0,0 +1,4 @@
+@echo off
+mkdir default_backup
+java -jar ./jars/Client.jar localhost 2000
+pause
