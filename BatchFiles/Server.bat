11e9e288bc7361a19ead5b5a466fbf0f18980327
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2023-11-17 18:26:56 +0000

    push (batch files removed)

--- BatchFiles/Server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd ..\Server_Cluster\src\main\java\
-
-javac pt\isec\pd\Main_Server.java
-
-java -cp "..\..\..\database\lib\sqlite-jdbc-3.43.0.0.jar;" pt.isec.pd.Main_Server 2000 ..\..\..\database\ default_rmi_service_name 6000
-
-pause
